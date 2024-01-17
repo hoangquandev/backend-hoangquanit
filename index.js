75df778242cffd1d6e09e0f9b5c0000e94a928df
Hoàng Quân <lehoangquanit@gmail.com>
2024-01-17 17:25:53 +0700

    #

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
